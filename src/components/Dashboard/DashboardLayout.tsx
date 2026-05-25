2c3fe5d9e22855a0e3d997c6e09894d0e577347d
gestaodeescalashcc <gestaodevisitanteshecc@gmail.com>
2026-05-25 11:44:21 -0300

    Update DashboardLayout.tsx

--- src/components/Dashboard/DashboardLayout.tsx
+++ src/components/Dashboard/DashboardLayout.tsx
@@ -578,7 +578,7 @@ export default function DashboardLayout({
           </div>
         </header>
 
-        <main className="flex-1 overflow-y-auto p-4 sm:p-6">{children}</main>
+        <main className="flex-1 overflow-y-auto p-3 sm:p-4 md:p-6">{children}</main>
       </div>
     </div>
   );
